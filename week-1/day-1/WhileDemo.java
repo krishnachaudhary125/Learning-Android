eaab054c227a7ce471645b11ff325c950e4ec7d9
krishnachaudhary125 <krishnachaudhary0125@gmail.com>
2026-06-29 17:40:17 +0545

    Use of while loop

---
+++ week-1/day-1/WhileDemo.java
@@ -0,0 +1,9 @@
+public class WhileDemo{
+    public static void main(String[] args){
+        int count = 1;
+        while(count <= 5){
+            System.err.println("Count : "+count);
+            count++;
+        }
+    }
+}
