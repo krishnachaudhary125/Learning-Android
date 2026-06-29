073b770bdf87bb4dc8faedf97497d11902e416b9
krishnachaudhary125 <krishnachaudhary0125@gmail.com>
2026-06-29 14:16:57 +0545

    Array example with for-each loop

---
+++ week-1/day-1/ArrayExample.java
@@ -0,0 +1,9 @@
+public class ArrayExample{
+    public static void main(String[] args){
+        String[] names = {"Krishna", "Subodh", "Sumit", "Sachin", "Manish"};
+
+        for (String n : names) {
+            System.out.println(n);
+        }
+    }
+}
