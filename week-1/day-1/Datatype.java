4f2b4130481d777aaa2ba9c1b8b35df2f6a6b2db
krishnachaudhary125 <krishnachaudhary0125@gmail.com>
2026-06-29 14:08:12 +0545

    datatype of java

---
+++ week-1/day-1/Datatype.java
@@ -0,0 +1,15 @@
+public class Datatype{
+    public static void main(String[] args){
+        int intNum = 1;
+        float floatNum = 2.0f;
+        double doubleNum = 3.0d;
+        byte b = 0;
+        short s = 100;
+        char character = 'C';
+        String greet = "Namaste";
+        long longValue = 100000000;
+        boolean result = true;
+
+        System.out.println("Int = "+intNum+"\nFloat = "+floatNum+"\nDouble = "+doubleNum+"\nByte = "+b+"\nShort = "+s+"\nChar = "+character+"\nString = "+greet+"\nLong = "+longValue+"\nBoolean = "+result);
+    }
+}
