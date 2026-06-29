37c0aa87824eb3d1dd3f441c704d4e9f5d5d61dc
krishnachaudhary125 <krishnachaudhary0125@gmail.com>
2026-06-29 14:27:51 +0545

    Use of arithmetic operators

---
+++ week-1/day-1/ArithmeticOperator.java
@@ -0,0 +1,26 @@
+public class ArithmeticOperator {
+    public static void main(String[] args) {
+        int a = 25;
+        int b = 5;
+
+        //Additive operator
+        int additive = a + b;
+        System.out.println("Additive oprator = "+additive);
+
+        //Subtraction operator
+        int subtraction = a - b;
+        System.out.println("Subtraction = "+subtraction);
+
+        //Multiplication operator
+        int multiplication = a * b;
+        System.out.println("Multiplication = "+multiplication);
+
+        //Division operator
+        int division = a / b;
+        System.out.println("Division = "+division);
+
+        //Remainder opeartor
+        int remainder = a % b;
+        System.out.println("Remainder = "+remainder);
+    }
+}
