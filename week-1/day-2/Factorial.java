ce2c2713922fb361f95ef18cd84c3b8efcd7de81
krishnachaudhary125 <krishnachaudhary0125@gmail.com>
2026-06-30 11:08:58 +0545

    factorial using recursion

---
+++ week-1/day-2/Factorial.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Factorial{
+
+    static int fact(int n){
+        if(n == 0)
+            return 1;
+
+        return n * fact(n-1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number to find factorial : ");
+        int n = sc.nextInt();
+
+        System.out.println("Factorial of "+n+" is : "+fact(n));
+    }
+}
