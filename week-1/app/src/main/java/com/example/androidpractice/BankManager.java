98417daea80c2e12fb9c095af7ae64851fafa413
krishnachaudhary125 <krishnachaudhary0125@gmail.com>
2026-07-01 13:52:55 +0545

    method to add account with account existence check

---
+++ week-1/app/src/main/java/com/example/androidpractice/BankManager.java
@@ -0,0 +1,23 @@
+package com.example.androidpractice;
+
+import java.util.ArrayList;
+
+public class BankManager {
+    ArrayList<Account> accounts = new ArrayList<>();
+
+    public boolean accountExist(int accountNumber){
+        for(Account account : accounts){
+            if(account.getAccountNumber() == accountNumber){
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean createAccount(Account account){
+        if(accountExist(account.getAccountNumber())){
+            return false;
+        }
+        accounts.add(account);
+        return true;
+    }
+}
