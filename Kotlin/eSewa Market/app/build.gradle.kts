b404642e6a6d0b5e2546a5b0063165e2ba9caba6
krishnachaudhary125 <krishnachaudhary0125@gmail.com>
2026-08-18 15:20:45 +0545

    feat: coil setup

--- Kotlin/eSewa Market/app/build.gradle.kts
+++ Kotlin/eSewa Market/app/build.gradle.kts
@@ -117,4 +117,8 @@ dependencies {
     implementation("androidx.compose.material3:material3")
 
     debugImplementation("androidx.compose.ui:ui-tooling")
+
+    // Coil setup
+    implementation("io.coil-kt.coil3:coil-compose:3.0.4")
+    implementation("io.coil-kt.coil3:coil-network-okhttp:3.0.4")
 }
