78feb3f9b737b188744f67be276413615d1f035e
krishnachaudhary125 <krishnachaudhary0125@gmail.com>
2026-08-18 10:34:23 +0545

    feat: favourite product card build with compose

--- Kotlin/eSewa Market/app/src/main/java/com/example/eSewaMarket/ui/compose/CleanIconButton.kt
+++ Kotlin/eSewa Market/app/src/main/java/com/example/eSewaMarket/ui/compose/CleanIconButton.kt
@@ -1,14 +1,16 @@
 package com.example.eSewaMarket.ui.compose
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
-import androidx.compose.material3.Icon
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun CleanIconButton(
@@ -26,9 +28,10 @@ fun CleanIconButton(
             ),
         contentAlignment = Alignment.Center
     ){
-        Icon(
+        Image(
             painter = painterResource(icon),
-            contentDescription = contentDescription
+            contentDescription = contentDescription,
+            modifier = Modifier.size(24.dp)
         )
     }
 }
