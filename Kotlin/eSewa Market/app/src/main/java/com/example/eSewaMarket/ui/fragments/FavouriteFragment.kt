80846528dde142f89c3f891223eab304eefdd57a
krishnachaudhary125 <krishnachaudhary0125@gmail.com>
2026-08-18 15:25:12 +0545

    feat: displayed the favourite products using compose

--- Kotlin/eSewa Market/app/src/main/java/com/example/eSewaMarket/ui/fragments/FavouriteFragment.kt
+++ Kotlin/eSewa Market/app/src/main/java/com/example/eSewaMarket/ui/fragments/FavouriteFragment.kt
@@ -4,11 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.eSewaMarket.ui.compose.FavouriteFragmentScreen
+import com.example.eSewaMarket.ui.factory.ViewModelFactoryProvider
+import com.example.eSewaMarket.ui.viewmodel.FavouriteViewModel
 
 class FavouriteFragment : Fragment() {
+    private val favouriteViewModel: FavouriteViewModel by viewModels {
+        ViewModelFactoryProvider.favouriteFactory(requireContext())
+    }
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -16,7 +25,29 @@ class FavouriteFragment : Fragment() {
     ): View {
         return ComposeView(requireContext()).apply {
             setContent {
-                FavouriteFragmentScreen()
+                val products by favouriteViewModel.favouriteProducts()
+                    .collectAsStateWithLifecycle(
+                        initialValue = emptyList()
+                    )
+
+                FavouriteFragmentScreen(
+                    products = products,
+                    onBackClick = {
+                        requireActivity()
+                            .onBackPressedDispatcher
+                            .onBackPressed()
+                    },
+                    noOfItems = {
+                        Text(
+                            text = "( ${products.size} )"
+                        )
+                    },
+                    deleteAll = {},
+                    onProductClick = {},
+                    onAddToCartClick = {},
+                    onOptionClick = {},
+                    onTickClick = {}
+                )
             }
         }
     }
