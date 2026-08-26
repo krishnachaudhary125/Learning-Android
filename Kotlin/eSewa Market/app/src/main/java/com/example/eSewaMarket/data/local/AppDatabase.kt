11dac4e17a62d61db4f2dddc3be77bfad8bd4f7a
krishnachaudhary125 <krishnachaudhary0125@gmail.com>
2026-08-26 14:48:22 +0545

    fix: upgraded the room version

--- Kotlin/eSewa Market/app/src/main/java/com/example/eSewaMarket/data/local/AppDatabase.kt
+++ Kotlin/eSewa Market/app/src/main/java/com/example/eSewaMarket/data/local/AppDatabase.kt
@@ -15,7 +15,7 @@ import com.example.eSewaMarket.data.local.entity.ProductEntity
         FavouriteEntity::class,
         ProductEntity::class
     ],
-    version = 2
+    version = 3
 )
 
 abstract class AppDatabase : RoomDatabase() {
